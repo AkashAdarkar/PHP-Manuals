6581a2b1cf216fbe5c145cfda973603868c52fe6
Akash Adarkar <aregret01@gmail.com>
2021-04-22 22:21:21 +0530

    practical no 4

---
+++ Experiment4/associative.php
@@ -0,0 +1,7 @@
+<?php
+$marks=array("Maths"=>12,"English"=>32,"Science"=>34);
+echo $marks['Maths']."<br/>".$marks['English']."<br/>".$marks['Science']."<br/>";
+echo is_array($marks)."<br>";
+var_dump($marks);
+
+?>
---
+++ Experiment4/indexedArr.php
@@ -0,0 +1,7 @@
+<?php
+$city=array(0=>"Pune",1=>"Mumbai",2=>"Delhi");
+echo $city[0]."<br/>".$city[1]."<br/>".$city[2]."<br/>";
+var_dump($city);
+
+echo "</br>".is_array($city);
+?>
